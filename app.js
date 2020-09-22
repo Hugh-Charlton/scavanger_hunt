cd989c4f248f6700dcd36e24ace1a84292ab8d0d
Hugh-Charlton <hugh_charlton@hotmail.co.uk>
2020-09-22 10:23:15 +1000

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-import { hPKey } from './app.js'
+import { hPKey } from './APIKey.js'
 
 let mySpellbook = []
 let counter = 0
+let num = 10
 
 function getItems() {
 
@@ -16,12 +17,34 @@ function getItems() {
         url: `https://www.potterapi.com/v1/spells` + "?key=" + hPKey,
     })
         .then(function (response){
-        // hard coded information from API
-        // console.log(response.data[0])
-        let num = randomNumber()
-        // adds new spells to the spellbook
-        mySpellbook.push(response.data[num].spell)
-        console.log(mySpellbook)
+            // hard coded information from API
+            // console.log(response.data[0])
+            // adds new spells to the spellbook
+            counter += 1
+            console.log(counter)
+
+            for (i = 0; i < num; i++) {
+                // adds a random spell to the list and then checks/removes dupes
+                mySpellbook.push(response.data[randomNumber()].spell)
+                mySpellbook = mySpellbook.filter((v, i) => mySpellbook.indexOf(v) === i)
+                console.log(mySpellbook)
+
+                // prevents infinte loops, via data entry
+                if (num > 20 || num * counter > 151) {
+                    console.log("bad luck!")
+                    break;
+                }
+
+                // removes 1 from i to make a loop not count when a dupe is created
+                if (i * counter > mySpellbook.length - (1 * counter)){
+                    console.log(i * counter)
+                    console.log(mySpellbook.length)
+                    i--
+                }
+            }
+
+        mySpellbook = mySpellbook.filter((v, i) => mySpellbook.indexOf(v) === i)
+
         // map function to add each new spell to the spellbook list
         let test = mySpellbook.map(x => `<li>${x}</li>`).join("")
 
